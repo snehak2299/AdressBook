4f8e9d5ae1adbe9ec8f021ce140687e27fc2aee8
snehak2299 <skanase2299@gmail.com>
2021-08-17 00:21:17 +0530

    [sneha]:added new feature to add more contact in address book

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-package com.bridglab.addressbook;
 
-public class AddressBook {
+package com.bridglab.addressbook;
+import java.util.Scanner;
 	
 	class Contacts{
 		String firstName;
@@ -25,23 +24,46 @@ public class AddressBook {
 			this.email = email;
 			this.zipCode = zipCode;
 			this.phoneNumber = phoneNumber;
+			System.out.println("contact added succesfully");
+			
 		}
-		
-		
 	}
 	
-	
-	public static void main(String[] args) {
-		System.out.println("welcome to address book program");
-=======
-package com.bridgelab.addressbook;
+	public class AddressBook {
 
-public class AddressBook {
+		public static void main(String[] args) {
+			System.out.println("welcome to address book");	//welcome message
+			Scanner sc = new Scanner(System.in); // taking input
+			System.out.println("how many records you want to add:");
+			int totalRecords=sc.nextInt();
+			Contacts[]arr = new Contacts[totalRecords]; //to store the added contacts
+			while(totalRecords!=0) {
+				System.out.println("enter first name: ");
+				// to stop going to next line adding dummy value
+				String dummy = sc.nextLine();
+	            String firstName = sc.nextLine();
+				System.out.println("enter last name:");
+				String lastName = sc.nextLine();
+				System.out.println("enter address: ");
+				String address = sc.nextLine();
+				System.out.println("enter city: ");
+				String city = sc.nextLine();
+				System.out.println("enter state");
+				String state= sc.nextLine();
+				System.out.println("enter email");
+				String email= sc.nextLine();
+				System.out.println("enter zip code:");
+				int zipCode = sc.nextInt(); 
+				System.out.println("enter phone number:");
+				int phoneNumber = sc.nextInt(); 
+				//creating new object
+				Contacts contact = new Contacts(firstName,lastName,address,city,state, email,zipCode, phoneNumber);
+				totalRecords--; 
+			}
+			
+			
+		}
+	}	
+		
+		
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to address book program");
->>>>>>> 924217e76c06144898623cc08bb04aefbe66c721
-
-	}
-
-}
