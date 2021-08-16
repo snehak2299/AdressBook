be5f769db6bf4eeb806b98ed9c7612d650437b26
snehak2299 <skanase2299@gmail.com>
2021-08-16 21:16:07 +0530

    [sneha]:added ability to add contact in address book

--- AddressBook.java
+++ AddressBook.java
@@ -1,9 +1,37 @@
 package com.bridglab.addressbook;
 
 public class AddressBook {
-
+	
+	class Contacts{
+		String firstName;
+		String lastName;
+		String address;
+		String city;
+		String state;
+		String email;
+		int zipCode;
+		int phoneNumber;
+		
+		//created constructor using filed 
+		public Contacts(String firstName, String lastName, String address, String city, String state, String email,
+				int zipCode, int phoneNumber) {
+			
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.address = address;
+			this.city = city;
+			this.state = state;
+			this.email = email;
+			this.zipCode = zipCode;
+			this.phoneNumber = phoneNumber;
+		}
+		
+		
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("welcome to address book program");
 
 	}
 
